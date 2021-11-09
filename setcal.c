f80c976928da1ef9ca8d880d99f850d0838f619a
Vlad_Malashchuk <vladok.malashuk@gmail.com>
2021-11-09 19:04:29 +0200

    Start2

--- setcal.c
+++ setcal.c
@@ -2,4 +2,26 @@
 #include <conio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+//
+bool IsEmpy(char A) {}
+int Card(char A) {}
+char Complement(char A) {}
+char Union(char A, char B) {}
+char Intersect(char A, char B) {}
+char Minus(char A, char B) {}
+bool Subseteq(char A, char B) {}
+bool Subset(char A, char B) {}
+bool Equals(char A, char B) {}
+//
+bool Reflexive(char R) {}
+bool Symmetric(char R) {}
+bool Antisymmetric(char R) {}
+bool Transitive(char R) {}
+bool Function(char R) {}
+bool Domain(char R) {}
+bool Codomain(char R) {}
+bool Injective(char R) {}
+bool Surjective(char R) {}
+bool Bijective(char R) {}
+//
 int main(int argc, char **argv) {}
