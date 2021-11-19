fe734701359eca032642892f27bc46a8fcba3e8c
pinlog33 <pinlog33@gmail.com>
2021-11-19 03:53:52 +0100

    commit5

--- setcal.c
+++ setcal.c
@@ -46,14 +46,14 @@ int fill_array(char*** array, char* source) {
         element[g+1] = '\0';
         g++;
         if (source[start+1] == '\n' || source[start+1] == '\0' || source[start+1] == ' ') {
-            char** check = (char**)realloc((*array), (31 * (f+2)));
+            char** check = (char**)realloc((*array), (31 * (f + 2)));
             if (!check) {
                 fprintf(stderr, "Smt is wrong ;(\n");
                 return 0;
             } else {
                 (*array) = check;
             }
-            (*array)[f+1] = (char*)malloc(31);
+            (*array)[f + 1] = (char*)malloc(31);
             strcpy((*array)[f], element);
             f++;                         
             clean(element);
@@ -77,9 +77,68 @@ typedef struct {
 } set;
 
 typedef struct {
-    char** elements;
+    char*** elements;
     int position;
 } relation;
+
+int fill_relation(char**** array, char* source) {
+    (*array) = (char***)malloc(1);
+    (*array)[0] = (char**)malloc(2);
+    (*array)[0][0] = (char*)malloc(31);
+    (*array)[0][1] = (char*)malloc(31);
+    
+    char element[31];
+    int g = 0;
+    int f = 0;
+    int r = -1;
+    int start = 2;
+    int control = 1;
+    while (source[start] != '\n' && g <= 30) {
+        control = 1;
+        if (source[start] == '(') {
+            r++;
+            f = 0;
+            control = 0;
+        } else {
+            if (source[start] != '(' && source[start] != ')' && source[start] != ' ') {
+                element[g] = source[start];
+                element[g+1] = '\0';
+                g++;
+            }
+        }
+        if (f > 2) {
+            fprintf(stderr, "Max amount of elements in relation is 2!");
+            return 0;
+        }
+        if ((source[start+1] == '\n' || source[start+1] == '\0' || (source[start+1] == ' ' &&  source[start+2] != '(')|| source[start+1] == ')') && control) {
+            if (f == 0) {
+                char*** check = (char***)realloc((*array), (62 * (r + 2)));
+                if (!check) {
+                    fprintf(stderr, "Smt is wrong ;(\n");
+                    return 0;
+                } else {
+                    (*array) = check;
+                    (*array)[r + 1] = (char**)malloc(2);
+                    (*array)[r + 1][f] = (char*)malloc(31);
+                    (*array)[r + 1][f + 1] = (char*)malloc(31);
+                }
+            }
+            strcpy((*array)[r][f], element);
+            f++;                         
+            clean(element);
+            g = 0;
+            if (source[start+1] == ')' && (source[start+2] == '\n' || source[start+2] == '\0')) {
+                break;
+            }
+        }
+        start++;
+    }
+    if (g > 30) {
+        fprintf(stderr, "Max length of argument is 30!\n");
+        return 0;
+    }
+    return 1;
+}
 
 int main(int argc, char **argv) {
     (void) argc;
@@ -93,6 +152,8 @@ int main(int argc, char **argv) {
     int am = 1;
     set* s1 = (set*)malloc(sizeof(set));
     int setc = 0;
+    relation* r1 = (relation*)malloc(sizeof(relation));
+    int relc = 0;
 
     while((buf = fgetc(f)) != EOF) {
         char* check = (char*)realloc(buffer, (c+2));
@@ -129,6 +190,20 @@ int main(int argc, char **argv) {
                     s1 = s2;
                 }
             }
+            if (buffer[0] == 'R') {
+                if (!fill_relation(&r1[relc].elements, buffer)) {
+                    return 1;
+                }
+                r1[relc].position = am;
+                relc++;
+                relation* r2 = (relation*)realloc(r1, sizeof(relation) * (relc + 1));
+                if (!r2) {
+                    fprintf(stderr, "Smt is wrong ;(\n");
+                    return 1;
+                } else {
+                    r1 = r2;
+                }
+            }
             printf("buffer: %s\n", buffer);
             clean(buffer);
             am++;
@@ -146,10 +221,26 @@ int main(int argc, char **argv) {
             printf("s1[%d].elements[%d]: %s\n", i, n, s1[i].elements[n]);
             free(s1[i].elements[n]);
         }
+        printf("s1[%d].position: %d\n", i, s1[i].position);
         free(s1[i].elements);
     }
 
+    for (int i = 0; i < relc; i++) {
+        for (int f = 0; r1[i].elements[f][0][0] != '\0'; f++) {
+            for (int g = 0; g < 2; g++) {
+                printf("r1[%d].elements[%d]: %s %s\n", i, f, r1[i].elements[f][g], r1[i].elements[f][g+1]);
+                free(r1[i].elements[f][g]);
+                g++;
+                free(r1[i].elements[f][g]);
+            }
+            free(r1[i].elements[f]);
+        }
+        printf("r1[%d].position: %d\n", i, r1[i].position);
+        free(r1[i].elements);
+    }
+
     free(uni);
+    free(r1);
     free(s1);
     free(buffer);
     fclose(f);
